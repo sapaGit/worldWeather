81ed42279dfa2d749106c7b6b02bcd89b09868dd
sapaGit <mosevaly@gmail.com>
2022-06-06 14:37:35 +0300

    Initial Commit

---
+++ worldWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  worldWeather
+//
+//  Created by Sergey Pavlov on 06.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
