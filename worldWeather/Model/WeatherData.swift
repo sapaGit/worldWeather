91e2d119dc94a9de197caa705d6a41b1b33dac8e
sapaGit <mosevaly@gmail.com>
2022-06-06 18:03:49 +0300

    01.5 Making WeatherData model from JSON

---
+++ worldWeather/Model/WeatherData.swift
@@ -0,0 +1,37 @@
+//
+//  WeatherData.swift
+//  worldWeather
+//
+//  Created by Sergey Pavlov on 06.06.2022.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let info: Info
+    let fact: Fact
+}
+struct Info: Decodable {
+    let url: String
+}
+
+struct Fact: Decodable {
+    let tempAvg: Int
+    let icon: String
+    let condition: String
+    let windSpeed: String
+    let pressureMm: Int
+    let tempMin: Int
+    let tempMax: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case tempAvg = "temp_avg"
+        case icon
+        case condition
+        case windSpeed = "wind_speed"
+        case pressureMm = "pressure_mm"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+    }
+}
+
