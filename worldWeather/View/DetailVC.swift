e249024676e46f1316103399716058964e39da69
sapaGit <mosevaly@gmail.com>
2022-06-08 11:00:04 +0300

    01.2 Install SVGPod. Creating Image from sag url.

--- worldWeather/View/DetailVC.swift
+++ worldWeather/View/DetailVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftSVG
 
 class DetailVC: UIViewController {
 
@@ -18,8 +19,28 @@ class DetailVC: UIViewController {
     @IBOutlet var humidityLabel: UILabel!
     @IBOutlet var feelsLikeLabel: UILabel!
     
+    
+    var weatherModel: Weather?
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    
+    func refreshLabels() {
+        nameCityLabel.text = weatherModel?.name
+        
+        guard let url = URL(string: "https://yastatic.net/weather/i/icons/funky/dark/\(String(describing: weatherModel?.condition)).svg)") else { return }
+        
+        let weatherImage = UIView(SVGURL: url) { image in
+            image.resizeToFit(self.viewCity.bounds)
+        }
+        self.viewCity.addSubview(weatherImage)
+        
+        conditionLabel.text = weatherModel?.condition
+        tempCityLabel.text = weatherModel?.tempString
+        pressureLabel.text = "\(String(describing: weatherModel?.pressureMm))"
+        windSpeedLabel.text = "\(String(describing: weatherModel?.windSpeed))"
+        humidityLabel.text = "\(String(describing: weatherModel?.humidity))"
+        feelsLikeLabel.text = "\(String(describing: weatherModel?.feelsLike))"
+    }
 }
